2412dd6da71458aa76397b56eeb58513ce4cca29
Mauricio Flor <duvamalicio3d@hotmail.com>
2019-02-07 22:11:11 -0500

    Unit jest

--- tests/unit/card.spec.js
+++ tests/unit/card.spec.js
@@ -1,19 +1,33 @@
-import { mount  } from '@vue/test-utils'
+import Vue from 'vue'
+// import { mount  } from '@vue/test-utils'
 import PmCard from '@/components/material/Card'
 
-describe('Component', () => {
-  it('renders a div', () => {
-    const wrapper = mount(PmCard, {
-      propsData: {
-        imgHeight: 212
-      }
+const mountComponentWithProps = (Component, propsData) => {
+  const Constructor = Vue.extend(Component);
+  const vm = new Constructor({
+    propsData
+  }).$mount();
+  return vm.$el;
+}
+
+describe('Card', () => {
+  it('should be the correct color', () => {
+    const cardData = mountComponentWithProps(PmCard, { 
+      color: 'blue' 
     })
-    expect(wrapper.props().imgHeight).toBe(300)
-  })
-  /*
-  test('is a Vue instance', () => {
-    const wrapper = mount(PmCard)
-    expect(wrapper.isVueInstance()).toMatchSnapshot()
-  })
-  */
-})
+    const styleData = cardData.style.getPropertyValue('color')
+
+    console.log('styleData => ', styleData)
+
+    expect(styleData).not.toBe({ color: 'red' });
+  });
+
+  it('should have the correct img', () => {
+    const cardData = mountComponentWithProps(PmCard, { img: 'https://images212.dog.ceo/breeds/lhasa/n02098413_8273.jpg' });
+    const titleData = cardData.textContent;
+
+    console.log(titleData)
+
+    expect(titleData).not.toBe('https://images.dog.ceo/breeds/lhasa/n02098413_8273.jpg');
+  });
+});
